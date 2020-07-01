aeba7a81eb62b560861b8ff0fee56a883efa33cc
Paul G Webster <paul.g.webster@googlemail.com>
2020-07-01 13:12:51 +0100

    Added hostport function

--- Magnum-OpusVL-CommandCommon/lib/Magnum/OpusVL/CommandCommon/Plugin/V1.pm
+++ Magnum-OpusVL-CommandCommon/lib/Magnum/OpusVL/CommandCommon/Plugin/V1.pm
@@ -28,7 +28,6 @@ use open qw(:std :utf8);
 use experimental qw(signatures);
 
 # External modules
-#use
 
 # Version of this software
 our $VERSION = '0.001';
@@ -39,7 +38,8 @@ sub new {
 
     my $self = bless {
         functions   =>  {
-            count_occurrences   =>  \&count_occurrences
+            count_occurrences   =>  \&count_occurrences,
+            extract_hostport    =>  \&extract_hostport,
         },
         version     =>  1,
     }, $class;
@@ -47,14 +47,43 @@ sub new {
     return $self;  
 }
 
+=head2 count_occurrences
+
+Count the occurences of a regex or string within a string, takes two arguments:
+
+    arg1: The match criteria as a qr// or string
+    arg2: The data to match against
+
+Will return the number of matches of arg1 in arg2.
+
+=cut
+
 sub count_occurrences($self,$match_criteria,$data) {
     if (!$match_criteria || !$data)  {
-        return;
+        die "Invalid arguments passed to function";
     }
 
     my $count = () = $data =~ /$match_criteria/g;
 
     return $count;
+}
+
+=head2 extract_hostport
+
+Extract the host/ip and port from a single word string, for example: 1.1.1.1:53
+
+Accepts 1 mandatory argument and 1 optional argument:
+
+    Arg1: The hostport set.
+    Arg2: The optional seperator, defaults to ':'.
+
+Will return a list of host and port.
+
+=cut
+
+sub extract_hostport($self,$hostport,$seperator = ':') {
+    my ($host,$port) = split(/$seperator/,$hostport,2);
+    return ($host,$port);
 }
 
 =head1 AUTHOR
